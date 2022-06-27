e48a70ff0ca26beb27de2e451193aea6c910a057
Ryan Ruenroeng <rruenroeng@gmail.com>
2022-06-26 23:05:08 -0500

    Modals + Delete Stream Additions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -44,7 +44,7 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
   history.push("/");
@@ -54,4 +54,5 @@ export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id });
+  history.push("/");
 };
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,15 +1,31 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { formValues } from "redux-form";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
-    console.log(this.props);
+    if (!this.props.stream) {
+      return <div>"Loading..."</div>;
+    }
+
     return (
-      <div>{this.props.stream ? this.props.stream.title : "Loading..."}</div>
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
+      </div>
     );
   }
 }
@@ -18,4 +34,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
